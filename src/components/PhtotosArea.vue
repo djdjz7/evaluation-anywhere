a72d13f1a71797b06dfd3ccf85e8072514283bed
Jason Dai <djdjz7@icloud.com>
2024-02-10 16:14:27 +0800

    upload images to canvas

--- src/components/PhtotosArea.vue
+++ src/components/PhtotosArea.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import { type AnswersToQstFlow } from "@/models/Answers";
 import { useUserInfoStore } from "@/stores/userInfo";
 import { CameraIcon, PhotoIcon } from "@heroicons/vue/24/outline";
@@ -11,15 +11,36 @@ const props = defineProps<{
 }>();
 
 const images: Blob[] = [];
-const imageSrc = ref<string[]>([]);
+const imageBitmaps = new Array<ImageBitmap>();
 const accpetedTypes = ["image/png", "image/jpg", "image/jpeg"];
 
 let lastUpdate: number = Date.now();
 
 const cameraInput = ref<HTMLInputElement | null>();
 const albumnInput = ref<HTMLInputElement | null>();
+const canvasRef = ref<HTMLCanvasElement | null>();
+let canvas: HTMLCanvasElement | null | undefined;
+let ctx: CanvasRenderingContext2D | null;
+
+const deltaHeight: number[] = [0];
+
+onMounted(() => {
+  canvas = canvasRef.value;
+  if (canvas == null) {
+    alert("未知错误，请刷新页面后重试。");
+    return;
+  }
+  ctx = canvas.getContext("2d");
+  if (ctx == null) {
+    alert("未知错误，请刷新页面后重试。");
+    return;
+  }
+});
 
 const getQstAnswerAsync = async (): Promise<AnswersToQstFlow> => {
+  const canvas = new HTMLCanvasElement();
+  canvas.width = 1600;
+
   // TODO: merge images and put object
   return {
     answers: [
@@ -37,8 +58,16 @@ function addImage(input: HTMLInputElement | null | undefined) {
   input?.click();
 }
 
-function handleImage(input: HTMLInputElement | null | undefined) {
+async function handleImage(input: HTMLInputElement | null | undefined) {
   if (input?.files == null) {
+    return;
+  }
+  if (canvas == null) {
+    alert("未知错误，请刷新页面后重试。");
+    return;
+  }
+  if (ctx == null) {
+    alert("未知错误，请刷新页面后重试。");
     return;
   }
   const file = input.files[0];
@@ -47,15 +76,52 @@ function handleImage(input: HTMLInputElement | null | undefined) {
     return;
   }
   images.push(file);
-  imageSrc.value.push(URL.createObjectURL(file));
+  const imageBitmap = await createImageBitmap(file);
+  imageBitmaps.push(imageBitmap);
+  if (imageBitmap.width <= 1600) {
+    deltaHeight.push(deltaHeight[deltaHeight.length - 1] + imageBitmap.height);
+  } else {
+    deltaHeight.push(
+      deltaHeight[deltaHeight.length - 1] + imageBitmap.height * (1600 / imageBitmap.width)
+    );
+  }
+
+  let totalHeight = 0;
+  imageBitmaps.forEach((x) => {
+    if (x.width <= 1600) {
+      totalHeight += x.height;
+    } else {
+      const resizedHeight = x.height * (1600 / x.width);
+      totalHeight += resizedHeight;
+    }
+  });
+
+  canvas.height = totalHeight;
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(0, 0, 1600, totalHeight);
+
+  imageBitmaps.forEach((x, index) => {
+    if (x.width <= 1600) {
+      const deltax = (1600 - x.width) / 2;
+      ctx!.drawImage(x, deltax, deltaHeight[index]);
+    } else {
+      const ratio = 1600 / x.width;
+      const resizedHeight = x.height * ratio;
+      ctx!.drawImage(x, 0, deltaHeight[index], 1600, resizedHeight);
+    }
+  });
   input.files = null;
+
+  console.log(imageBitmaps);
+  console.log(deltaHeight);
+  console.log(totalHeight);
 }
 
 defineExpose({ getQstAnswerAsync });
 </script>
 
 <template>
-  <img v-for="image in imageSrc" :src="image" w-full />
+  <canvas width="1600" height="0" ref="canvasRef"></canvas>
   <input
     capture="environment"
     un-hidden
